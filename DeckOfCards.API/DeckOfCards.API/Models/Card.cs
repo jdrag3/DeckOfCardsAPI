abef5df9434be88e4d98a4da335b8fe5f49bcebf
J R <clearlight@msn.com>
2018-02-05 16:22:30 +0000

    Merged PR 5: adding unit tests
    adding unit tests

--- DeckOfCards.API/DeckOfCards.API/Models/Card.cs
+++ DeckOfCards.API/DeckOfCards.API/Models/Card.cs
@@ -10,5 +10,36 @@
 
         public string Suit { get; set; }
         public string Face { get; set; }
+
+        public static bool operator ==(Card card1, Card card2)
+        {
+            if ((card1.Face.CompareTo(card2.Face) == 0) &&
+                (card1.Suit.CompareTo(card2.Suit) == 0))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool operator !=(Card card1, Card card2)
+        {
+            return !(card1 == card2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Card)
+            {
+                Card card = obj as Card;
+                return (this == card);
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
